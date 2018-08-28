2c9b690cd64f998fad273d60eb0c69cff0cb7538
Raul Canul <eduardoct120@gmail.com>
2018-08-28 12:07:09 -0500

    registerPressed function implemented on RegisterViewController

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -32,7 +32,18 @@ class RegisterViewController: UIViewController {
 
         
         //TODO: Set up a new user on our Firbase database
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("success")
+                
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+            
+        }
         
 
         
